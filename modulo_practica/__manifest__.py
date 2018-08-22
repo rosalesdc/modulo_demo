f76c5940f9ab3ba99529bd4491e9658a84981a8b
davidr <rosales9146@gmail.com>
2018-08-22 16:04:56 -0500

    [ADD]ALL: Agregados archivos de modulo de practica

---
+++ modulo_practica/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Modulo de practica",
+
+    'summary': """
+    """,
+
+    'description': """
+        Modulo creado como practica
+    """,
+
+    'author': "Soluciones4G",
+    'website': "http://www.soluciones4g.com",
+
+    # Categories can be used to filter modules in modules listing
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base',
+    ],
+
+    # always loaded
+    'data': [
+        'views/refaccion_view.xml',
+    ],
+    'installable':True,
+}
